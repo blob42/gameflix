d7f38cbbc8f4718296661bc6e83646268b3bd159
Chakib Benziane <chakib.benz@gmail.com>
2015-10-04 15:59:55 +0200

    replace ResourceLoader with ViewLoader

--- client/js/application.js
+++ client/js/application.js
@@ -9,62 +9,95 @@ This is the entry point to the application and handles the initial loading of re
 */
 
 var resourceLoader;
+var viewLoader;
 
 /**
- * @description The onLaunch callback is invoked after the application JavaScript 
- * has been parsed into a JavaScript context. The handler is passed an object 
+ * @description The onLaunch callback is invoked after the application JavaScript
+ * has been parsed into a JavaScript context. The handler is passed an object
  * that contains options passed in for launch. These options are defined in the
  * swift or objective-c client code. Options can be used to communicate to
- * your JavaScript code that data and as well as state information, like if the 
+ * your JavaScript code that data and as well as state information, like if the
  * the app is being launched in the background.
  *
- * The location attribute is automatically added to the object and represents 
+ * The location attribute is automatically added to the object and represents
  * the URL that was used to retrieve the application JavaScript.
  */
 App.onLaunch = function(options) {
+
     var javascriptFiles = [
-        `${options.BASEURL}js/ResourceLoader.js`,
+        `${options.BASEURL}js/ViewLoader.js`,
         `${options.BASEURL}js/Presenter.js`
     ];
+
+
+
+
+
+
 
     /**
      * evaluateScripts is responsible for loading the JavaScript files neccessary
      * for you app to run. It can be used at any time in your apps lifecycle.
-     * 
-     * @param - Array of JavaScript URLs  
+     *
+     * @param - Array of JavaScript URLs
      * @param - Function called when the scripts have been evaluated. A boolean is
      * passed that indicates if the scripts were evaluated successfully.
      */
     evaluateScripts(javascriptFiles, function(success) {
         if (success) {
-            resourceLoader = new ResourceLoader(options.BASEURL);
+            //resourceLoader = new ResourceLoader(options.BASEURL);
+            var viewLoader = new ViewLoader(options.APIURL);
 
-            var index = resourceLoader.loadResource(`${options.BASEURL}templates/Index.xml.js`, 
-                function(resource) {
-                    var doc = Presenter.makeDocument(resource);
-                    doc.addEventListener("select", Presenter.load.bind(Presenter));
-                    navigationDocument.pushDocument(doc);
-                });
+            viewLoader.load('index', function(resource){
+                var doc = Presenter.makeDocument(resource);
+                doc.addEventListener("select", Presenter.load.bind(Presenter));
+                navigationDocument.pushDocument(doc);
+            })
+
         } else {
-            /*
-            Be sure to handle error cases in your code. You should present a readable, and friendly
-            error message to the user in an alert dialog.
+            //Be sure to handle error cases in your code. You should present a readable, and friendly
+            //error message to the user in an alert dialog.
 
-            See alertDialog.xml.js template for details.
-            */
+            //See alertDialog.xml.js template for details.
+            //*/
             var alert = createAlert("Evaluate Scripts Error", "There was an error attempting to evaluate the external JavaScript files.\n\n Please check your network connection and try again later.");
             navigationDocument.presentModal(alert);
 
             throw ("Playback Example: unable to evaluate scripts.");
         }
     });
+    ///////////////////////////////////////////
+    //   Test xmlhttprequest
+    ///////////////////////////////////////////
+    //var Requester = {
+        //handleResponse: function() {
+            ////console.log(this);
+            //var doc = Presenter.makeDocument(this.request.responseText);
+            //console.log(doc);
+        //},
+        //getIndex: function() {
+            //var self = this;
+
+            //if (!self.request) {
+                //console.log('setting up request')
+                //self.request = new XMLHttpRequest();
+            //}
+
+            //self.request.addEventListener("load", self.handleResponse.bind(self));
+            //self.request.open("GET", `${options.BASEURL}templates/Index.xml.js`)
+            //self.request.send();
+        //}
+    //}
+
+    //Requester.getIndex();
+
 }
 
 
 /**
  * This convenience funnction returns an alert template, which can be used to present errors to the user.
  */
-var createAlert = function(title, description) {  
+var createAlert = function(title, description) {
 
     var alertString = `<?xml version="1.0" encoding="UTF-8" ?>
         <document>
