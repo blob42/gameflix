ad18f96c659c9440f06a561e450a18cfbcb9b051
Chakib Benziane <chakib.benz@gmail.com>
2015-10-04 11:47:19 +0200

    gameflix initial commit

---
+++ GameFlix/GameFlixTests/GameFlixTests.swift
@@ -0,0 +1,36 @@
+//
+//  GameFlixTests.swift
+//  GameFlixTests
+//
+//  Created by spike on 04/10/15.
+//  Copyright © 2015 Marbelium. All rights reserved.
+//
+
+import XCTest
+@testable import GameFlix
+
+class GameFlixTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ server.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+#echo "./server.sh 9042"
+
+python -m SimpleHTTPServer 9042
