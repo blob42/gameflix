c1c5b8f9beb0116a2963314f7da08aeff740275a
Chakib Benziane <chakib.benz@gmail.com>
2015-10-05 00:23:09 +0200

    api and client routes + xml templates redering

--- server.go
+++ server.go
@@ -1,15 +1,47 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"text/template"
 )
 
-func main() {
-	r := gin.Default()
-	r.GET("/test", func(c *gin.Context) {
-		c.String(200, "hello")
-	})
+//View name -> address mappings
+const (
+	TEMPLATES_DIR = "client/templates/index.xml"
+)
 
-	r.Run(":8080")
+var Views = map[string]string{
+	"index": "client/templates/index.xml",
+}
+
+func renderXMLTemplate(xmlpath string, c *gin.Context) {
+	t, _ := template.ParseFiles(xmlpath)
+	t.Execute(c.Writer, nil)
+}
+
+func handleView(c *gin.Context) {
+	viewName := c.Param("viewName")
+	xmlpath := Views[viewName]
+	renderXMLTemplate(xmlpath, c)
+}
+
+func main() {
+	router := gin.Default()
+
+	api := router.Group("/api")
+	{
+		api.GET("/test", func(c *gin.Context) {
+			c.String(http.StatusOK, "OK")
+		})
+	}
+
+	client := router.Group("/client")
+	{
+		client.Static("/js", "./client/js")
+		client.GET("/views/:viewName", handleView)
+	}
+
+	router.Run(":9042")
 }
